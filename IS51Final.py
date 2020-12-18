d7c27038b3fd2948464a85b5207e8bc98dbe49ad
andrewmoualol <70217928+andrewmoualol@users.noreply.github.com>
2020-12-17 23:44:52 -0800

    Update IS51Final.py

--- IS51Final.py
+++ IS51Final.py
@@ -6,3 +6,31 @@ by number of students grade higher than (83.25) / total students.
 
 
 """
+"""
+import file
+
+    num1 = input(total_numbers_of_students)
+    num2 = input(total_scores)
+    num3 = input(class_average)
+    num4 = input(number_of_students_score_above_class_average)
+    num5 = input(percentage_of_grades_above_average)
+
+    num3 = num1 / num 2
+    num5 = num4 / 1
+
+"""
+import fileinput
+
+def calculate_average():
+
+    text = open(r'Final.txt').read()
+    num_list = []
+
+    with open('Final.txt', 'r') as f:
+        for line in f:
+            num = line.strip()
+            num_list += [num]
+            return (num_list)
+
+
+main()
