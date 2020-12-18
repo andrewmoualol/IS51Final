07291b1328f9704a81e7c28476d4f37222b7c928
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-17 23:40:13 -0800

    add num comb

--- IS51Final.py
+++ IS51Final.py
@@ -2,7 +2,20 @@
 Open Final.txt, read Final.txt. Find the number of grades (print "24"), 
 find the average grades (print "83.25") by total score / number of students ("24")
 Numbers of students grade above (83.25) (print "13" students out of (24) = 54.17%) 
-by number of students grade higher than (83.25) / total students.
+by number of students grade higher than (83.25) / total students = Percentage of grades above average.
 
+"""
+
+"""
+import file
+
+    num1 = input(total_numbers_of_students)
+    num2 = input(total_scores)
+    num3 = input(class_average)
+    num4 = input(number_of_students_score_above_class_average)
+    num5 = input(percentage_of_grades_above_average)
+
+    num3 = num1 / num 2
+    num5 = num4 / 1
 
 """
