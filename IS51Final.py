c65e7553908f7cd333ad423e274c2bc58e717b31
Andrew Moua <andrewmoualol@mail.fresnostate.edu>
2020-12-17 23:08:10 -0800

    add structure-english

---
+++ IS51Final.py
@@ -0,0 +1,8 @@
+"""
+Open Final.txt, read Final.txt. Find the number of grades (print "24"), 
+find the average grades (print "83.25") by total score / number of students ("24")
+Numbers of students grade above (83.25) (print "13" students out of (24) = 54.17%) 
+by number of students grade higher than (83.25) / total students.
+
+
+"""
