ef016b7b07c3386f70bd58a72bcbb48b4ef4c203
andrewmoualol <70217928+andrewmoualol@users.noreply.github.com>
2020-12-17 23:24:27 -0800

    Update IS51Final.py

--- IS51Final.py
+++ IS51Final.py
@@ -2,7 +2,7 @@
 Open Final.txt, read Final.txt. Find the number of grades (print "24"), 
 find the average grades (print "83.25") by total score / number of students ("24")
 Numbers of students grade above (83.25) (print "13" students out of (24) = 54.17%) 
-by number of students grade higher than (83.25) / total students.
+by number of students grade higher than (83.25) / total students = Percentage of grades above average.
 
 
 """
